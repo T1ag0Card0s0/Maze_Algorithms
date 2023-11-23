3b79ad25559e853208a2de6fe45f72f0ac93558d
T1ag0Card0s0 <t.rafael03.tc@gmail.com>
2023-11-23 12:45:23 +0000

    add

--- MazeAlgorithms/src/mazeAlgorithms/ui/Controller.java
+++ MazeAlgorithms/src/mazeAlgorithms/ui/Controller.java
@@ -3,10 +3,8 @@ package mazeAlgorithms.ui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 
 public class Controller {
-    private Object userData;
 
     @FXML
     private Label label;
@@ -16,8 +14,6 @@ public class Controller {
     public void initialize() {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        Stage stage = (Stage) stackPane.getScene().getWindow();
-        this.userData=stage.getUserData();
         label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
     }
 }
